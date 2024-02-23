aadc460e63bc2feaced4df9096ec849297d796c1
chemashish <75088423+chemashish@users.noreply.github.com>
2024-02-23 09:58:12 +0000

    coversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,17 @@
+// let score=null
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLogged="ashish"
+
+console.log(typeof isLogged)
+
+let isLoggedInBoolean=Boolean (isLogged)
+console.log(typeof isLoggedInBoolean)
+
+console.log(isLoggedInBoolean)
